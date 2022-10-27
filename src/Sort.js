47c13f89cb8d15cbe4787b08d75fb5828e646b64
Rene Rubalcava <rrubalcava@esri.com>
2022-10-27 22:47:25 +0000

    Update 3 files

--- src/Sort.js
+++ src/Sort.js
@@ -4,13 +4,29 @@ import { App, snapshot, done, clear } from "./sort-visualizer";
 
 import "./sort.css";
 
-function sort(array) {
+function sort(nums) {
   // do cool stuff here
 
+  let swapped = false;
+
+  do {
+    swapped = false;
+    for (let i = 0; i < nums.length; ++i) {
+      snapshot(nums);
+      if (nums[i + 1] && nums[i] > nums[i + 1]) {
+        const temp = nums[i];
+        nums[i] = nums[i + 1];
+        nums[i + 1] = temp;
+        swapped = true;
+      }
+    }
+  } while (swapped);
+
+  // return nums;
   // call snapshot any time you do anything to the array
   // it's okay if you call it with duplicate value array,
   // it will deduplicate for you
-  snapshot(array);
+  snapshot(nums);
 }
 
 export default function SortComponent() {
